b167ba975973515d1008f7295838791593c4417e
Satya <nayak.satya@mahindra.com>
2022-12-06 16:34:13 +0530

    videohub commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  VideoDetail,
+  Feed,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
-function App() {
+const App = () => {
+  // const [data, setData] = useState([])
+  // console.log(data)
+  // useEffect(async () => {
+  //   const { data } = await axios.get("https://applicationfeedbackservice-dev.m-devsecops.com/backend/app/list",
+  //               {
+  //                   headers: {
+  //                       "Authorization": `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY3MDI1ODA1MywiaWF0IjoxNjcwMjQwMDUzfQ.Rdu7ndwKEDh8tFlHViUeLJrWj7a3v1k7882vdZrU_XvEHNCCKSSfy0ErA4aqu-hsMTLEZWUB7KyucCqRvvQOag`
+  //                   }, 
+                 
+  //     })
+         
+  //   setData(data)
+  // },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  )
 }
 
 export default App;
